aaac083afca78328ca4a5bb1e73bc4e85c973e54
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-04-26 11:03:51 +0200

    line 157 fix

--- detr/datasets/face.py
+++ detr/datasets/face.py
@@ -154,5 +154,5 @@ def build(image_set, args):
     }
 
     img_folder, ann_file = PATHS[image_set]
-    dataset = FaceDetection(img_folder, ann_file, transforms=make_Face_transforms(image_set), return_masks=args.masks)
+    dataset = FaceDetection(img_folder, ann_file, transforms=make_face_transforms(image_set), return_masks=args.masks)
     return dataset
